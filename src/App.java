671ce77dcdcdf43b4664dade410b2f0c9ae828df
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2022-07-31 15:32:54 -0300

    Commit inicial

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int studentAge = 15;
+        double gpa = 3.5;
+        char studentFirstInitial = 'J';
+        char StudentLastInitial = 'H';
+        boolean hasPerfectAteendance = true;
+
+        System.out.println(studentAge);
+        System.out.println(gpa);
+        System.out.println(studentFirstInitial);
+        System.out.println(StudentLastInitial);
+        System.out.println(hasPerfectAteendance);
+
+    }
+}
