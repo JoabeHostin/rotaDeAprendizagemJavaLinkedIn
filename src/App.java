48f9d90d8af4230b97389181b713f42a76434424
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2022-08-01 08:48:10 -0300

    atualiazação dos exercícios

--- src/App.java
+++ src/App.java
@@ -1,16 +1,27 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        int studentAge = 15;
-        double gpa = 3.5;
-        char studentFirstInitial = 'J';
-        char StudentLastInitial = 'H';
-        boolean hasPerfectAteendance = true;
 
-        System.out.println(studentAge);
-        System.out.println(gpa);
-        System.out.println(studentFirstInitial);
-        System.out.println(StudentLastInitial);
-        System.out.println(hasPerfectAteendance);
+import java.util.Scanner;
+
+public class App {
+
+    public static void main(String[] args) throws Exception {
+
+        try (Scanner input = new Scanner(System.in)) {
+            int studentAge = 28;
+
+            boolean hasPerfectAttendance = true;
+            String studentFirstName = "Joabe";
+            String studentLastName = "Hostin";
+            double gpa = input.nextDouble();
+            char studentFirstInitial = studentFirstName.charAt(0);
+            char studentFirstLastName = studentLastName.charAt(0);
+
+            System.out.println(studentAge);
+            System.out.println(gpa);
+            System.out.println(hasPerfectAttendance);
+            System.out.printf("%s %s %n", studentFirstName, studentLastName);
+            System.out.println(studentFirstInitial);
+            System.out.println(studentFirstLastName);
+        }
 
     }
 }
