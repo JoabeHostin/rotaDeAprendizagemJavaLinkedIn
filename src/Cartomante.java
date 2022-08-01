70aa12ec2beab708b739725ea4b4f9b5d5c89967
JoabeHostin <105891888+JoabeHostin@users.noreply.github.com>
2022-08-01 11:05:37 -0300

    update

---
+++ src/Cartomante.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+public class Cartomante {
+/**
+ *  @author: Joabe Hostin
+ * **/
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Bem vindo a cartomante!");
+        System.out.println("digite um número de 1 a 9");
+        
+        int numero = input.nextInt();
+
+        if(numero >= 1 && numero <= 9){
+
+
+        if (numero < 5){
+            System.out.println("Você terá um ótimo dia");
+        }else{
+            System.out.println("Você terá um dia ruim");
+        }
+        
+    }else{
+        System.out.println("Você digitou um número inválido");
+    }
+    }
+    }
+
---
+++ src/LoopWhile.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;   
+
+public class LoopWhile {
+
+public static void main(String[] args){
+
+
+    Scanner input = new Scanner(System.in);
+    boolean isOnrepeat = true;
+
+    while(isOnrepeat){
+
+        
+    }
+}
+
+}
